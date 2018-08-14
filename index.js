4a86c0280e2c190697ae48df3960cb42b4bb16f2
jellynina <jellynina@gmail.com>
2018-08-14 17:57:40 +0800

    solve row data formate not unit problem

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var fs = require("fs");
-var text = fs.readFileSync("./NS_RawData.txt").toString('utf-8');
+var text = fs.readFileSync("./UD_RawData.txt").toString('utf-8');
 text = text.replace(/\r\n/gi, '\r\n');
 var textByLine = text.split("\r\n");
 var finalArray = [];
 for (i in textByLine) {
   var tempString = textByLine[i].toString('utf-8').slice(2);
-  var buffA = tempString.split("   ");
-  //var buffA = tempString.split("\s+++");
+  var buffA = tempString.split(" ");
+  buffA = buffA.filter(word => word.length > 1);
+  for(j in buffA){
+    buffA[j] = buffA[j].toString('utf-8').replace(' ', '');
+  }
+
   finalArray = finalArray.concat(buffA);
 }
 
-var newFile = fs.createWriteStream('SL_NS_RawData.txt');
+var newFile = fs.createWriteStream('SL_UD_RawData.txt');
 
 newFile.on('error', function (err) { });
 finalArray.forEach(function (v) {
